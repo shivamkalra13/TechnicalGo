c23edeae6ccd0e7606e5d9027b1b620fd8f62216
shivamkalra13 <shivamkalra13@gmail.com>
2019-07-02 01:56:53 +0530

    Configured auto loading of Class and Subject filters

--- first_app/models.py
+++ first_app/models.py
@@ -1,36 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class Classes(models.Model):
+class Subject(models.Model):
+    class_names = ['1-8','9','10','11','12','Engineering','CA','B.Sc']
     CLASS_CHOICES = [               #List of tuple to provide specific choices in 'classno' field.
         ('1-8','1-8'),
         ('9','9'),
         ('10','10'),
         ('11','11'),
         ('12','12'),
-        ('engg','engg'),
-        ('ca','ca'),
-        ('bsc','bsc')
+        ('Engineering','Engineering'),
+        ('CA','CA'),
+        ('B.Sc','B.sc')
     ]
+    subcode = models.CharField(max_length = 7, blank = False, primary_key = True)
+    subname = models.CharField(max_length = 50, blank = False)
     classno = models.CharField(
     max_length = 15,
     choices = CLASS_CHOICES,
     default = '12')
-    subcode = models.CharField(max_length = 7, blank = False)
-
-    def __str__(self):
-        return str(self.classno)+","+str(self.subcode)
-
-class Subject(models.Model):
-    subcode = models.CharField(max_length = 7, blank = False, primary_key = True)
-    subname = models.CharField(max_length = 50, blank = False)
-    classno = models.ForeignKey(Classes, blank = False, on_delete = models.CASCADE, default='12')
 
     def __str__(self):
         return str((self.subcode,self.subname))
 
 class Notes(models.Model):
-    classno = models.ForeignKey(Classes, blank = False, on_delete = models.CASCADE, default='12')
     subcode = models.ForeignKey(Subject, blank = False, on_delete = models.CASCADE)
     skey = models.CharField(max_length = 100)     #SearchKey
     title = models.CharField(max_length = 100, blank = False)
@@ -41,7 +34,6 @@ class Notes(models.Model):
         return (self.title)
 
 class Videos(models.Model):
-    classno = models.ForeignKey(Classes, blank = False, on_delete = models.CASCADE, default='12')
     subcode = models.ForeignKey(Subject, blank = False, on_delete = models.CASCADE)
     skey = models.CharField(max_length = 100)     #SearchKey
     title = models.CharField(max_length = 100, blank = False)
@@ -52,7 +44,6 @@ class Videos(models.Model):
         return (self.title)
 
 class Books(models.Model):
-    classno = models.ForeignKey(Classes, blank = False, on_delete = models.CASCADE, default='12')
     subcode = models.ForeignKey(Subject, blank = False, on_delete = models.CASCADE)
     skey = models.CharField(max_length = 100)     #SearchKey
     title = models.CharField(max_length = 100, blank = False)
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Classes
 from first_app.models import Subject
 
 # Create your views here.
@@ -12,15 +11,23 @@ def index(request):
 
 def classes(request):
     GetParams = dict(request.GET)       #dictionary of GET parameters
-    sub_list=[]
-    if(len(GetParams.keys())==1):
-        cld = GetParams['cld']          #getting classDropdown parameter
-        all_classes = Classes.objects.filter(classno = cld[0]).values('subcode')
-        for i in all_classes:
-            sub_list.append(Subject.objects.filter(subcode=i['subcode']).values('subname')[0]['subname'])
-        print(sub_list)
+    sub_list = []                        #If no parameter is found then this empty list will pass
+    #class_list = Subject.objects.all().values('classno').distinct() #Get all distinct classno from db
+    class_list = Subject.class_names
+    cld = None                          #If no parameter is found then this None will pass
+    sbd = None                          #If no parameter is found then this None will pass
+    if(len(GetParams.keys())==1):       #Only class selected not subject
+        cld = GetParams['cld'][0]          #getting classDropdown parameter
+        sub_list = Subject.objects.filter(classno = cld)
+    elif(len(GetParams.keys())==2):      #Both class and subject selected
+        cld = GetParams['cld'][0]        #getting classDropdown parameter
+        sbd = GetParams['sbd'][0]        #getting subjectDropdown parameter
+        sub_list = Subject.objects.filter(classno = cld)
     mycontext={
-        'sub_list':sub_list
+        'sub_list':sub_list,     #List of subjects corresponding to classes
+        'class_list':class_list,
+        'sel_class':cld,
+        'sel_sub':sbd
     }
     return render(request, 'first_app/classes.html', context=mycontext)
 
