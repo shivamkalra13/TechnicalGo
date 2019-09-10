f6a57640925fbd46d127f16a10640be147db904b
Rohan-Singhal <r199819@gmail.com>
2019-09-10 14:01:18 +0530

    autoloading of notes accomplished

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Subject
+from first_app.models import Notes
+from first_app.models import Videos
+from first_app.models import Books
+
+#Some Auxiliary functions
+def getNotes(sub,cls):
+    notesLst = list()
+    if(sub and cls):
+        subcode_selected = (Subject.objects.filter(classno = cls).filter(subname = sub))[0].subcode
+        notesLst = Notes.objects.filter(subcode = subcode_selected)
+    elif(cls):
+        queryset = Subject.objects.filter(classno = cls)
+        for obj in queryset:
+            notesLst.extend(list(Notes.objects.filter(subcode = obj.subcode)))
+    else:
+        notesLst = Notes.objects.all()
+    return notesLst
 
 # Create your views here.
 def index(request):
@@ -13,9 +30,9 @@ def classes(request):
     GetParams = dict(request.GET)       #dictionary of GET parameters
     sub_list = []                        #If no parameter is found then this empty list will pass
     #class_list = Subject.objects.all().values('classno').distinct() #Get all distinct classno from db
-    class_list = Subject.class_names
-    cld = None                          #If no parameter is found then this None will pass
-    sbd = None                          #If no parameter is found then this None will pass
+    class_list = Subject.class_names    #list of all the class names in Subject model
+    cld = None                          #If no parameter is found then this None will pass(selected class)
+    sbd = None                          #If no parameter is found then this None will pass(selected subject)
     if(len(GetParams.keys())==1):       #Only class selected not subject
         cld = GetParams['cld'][0]          #getting classDropdown parameter
         sub_list = Subject.objects.filter(classno = cld)
@@ -23,11 +40,16 @@ def classes(request):
         cld = GetParams['cld'][0]        #getting classDropdown parameter
         sbd = GetParams['sbd'][0]        #getting subjectDropdown parameter
         sub_list = Subject.objects.filter(classno = cld)
+
+    #getting the notes objects list according to the selected suject and class.
+    notes_lst = getNotes(sbd,cld)
+
     mycontext={
         'sub_list':sub_list,     #List of subjects corresponding to classes
         'class_list':class_list,
         'sel_class':cld,
-        'sel_sub':sbd
+        'sel_sub':sbd,
+        'notes_list':notes_lst
     }
     return render(request, 'first_app/classes.html', context=mycontext)
 
