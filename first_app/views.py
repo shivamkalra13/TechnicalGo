dd1e661bee16b6913ee500a3ac3d5deac8c03243
shivamkalra13 <shivamkalra13@gmail.com>
2019-10-28 01:43:05 +0530

    1.Configured the videos and books pane content also 2.Configured the auto loop pics of notes and books

--- first_app/views.py
+++ first_app/views.py
@@ -19,6 +19,32 @@ def getNotes(sub,cls):
         notesLst = Notes.objects.all()
     return notesLst
 
+def getBooks(sub,cls):
+    booksLst = list()
+    if(sub and cls):
+        subcode_selected = (Subject.objects.filter(classno = cls).filter(subname = sub))[0].subcode
+        booksLst = Books.objects.filter(subcode = subcode_selected)
+    elif(cls):
+        queryset = Subject.objects.filter(classno = cls)
+        for obj in queryset:
+            booksLst.extend(list(Books.objects.filter(subcode = obj.subcode)))
+    else:
+        booksLst = Books.objects.all()
+    return booksLst
+
+def getVideos(sub,cls):
+    videosLst = list()
+    if(sub and cls):
+        subcode_selected = (Subject.objects.filter(classno = cls).filter(subname = sub))[0].subcode
+        videosLst = Videos.objects.filter(subcode = subcode_selected)
+    elif(cls):
+        queryset = Subject.objects.filter(classno = cls)
+        for obj in queryset:
+            videosLst.extend(list(Videos.objects.filter(subcode = obj.subcode)))
+    else:
+        videosLst = Videos.objects.all()
+    return videosLst
+
 # Create your views here.
 def index(request):
     mycontext={
@@ -43,13 +69,17 @@ def classes(request):
 
     #getting the notes objects list according to the selected suject and class.
     notes_lst = getNotes(sbd,cld)
+    books_lst = getBooks(sbd,cld)
+    videos_lst = getVideos(sbd,cld)
 
     mycontext={
         'sub_list':sub_list,     #List of subjects corresponding to classes
         'class_list':class_list,
         'sel_class':cld,
         'sel_sub':sbd,
-        'notes_list':notes_lst
+        'notes_list':notes_lst,
+        'books_list':books_lst,
+        'videos_list':videos_lst
     }
     return render(request, 'first_app/classes.html', context=mycontext)
 
